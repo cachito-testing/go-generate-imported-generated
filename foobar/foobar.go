27ad22ae910c3282fb09bfc7f8b6334f56bd06d9
Daniel Cho <dacho@redhat.com>
2021-10-20 20:29:15 -0500

    Initial commit

---
+++ foobar/foobar.go
@@ -0,0 +1,9 @@
+package foobar
+
+	import "fmt"
+
+	func Output() {
+	fmt.Println("booking", "BOOKING_TABLE_NAME")
+fmt.Println("foobar", "BARFOO_TABLE_NAME")
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/cachito-testing/go-generate-imported-generated
+
+go 1.17
